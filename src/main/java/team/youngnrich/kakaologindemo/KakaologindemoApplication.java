e0dd4fc795f5548eee791439f756e29a984a410b
Haeun Noh <00blowup@gmail.com>
2024-06-07 16:08:13 +0900

    Initial commit

---
+++ src/main/java/team/youngnrich/kakaologindemo/KakaologindemoApplication.java
@@ -0,0 +1,13 @@
+package team.youngnrich.kakaologindemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaologindemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakaologindemoApplication.class, args);
+	}
+
+}
